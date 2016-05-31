a05c4a8c58e443babdcda44681fdeb67fca5c37e
JM Han <gitbaow@163.com>
2016-05-31 08:43:04 -0700

    update

--- FirstLineofCode/ActivityLifeCycleTest/app/src/main/java/com/han/jm/activitylifecycletest/MainActivity.java
+++ FirstLineofCode/ActivityLifeCycleTest/app/src/main/java/com/han/jm/activitylifecycletest/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends Activity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 
+
 		if(savedInstanceState != null){
 			String tmpData = savedInstanceState.getString("data_key");
 			Log.d(TAG, tmpData);
